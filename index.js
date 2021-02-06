e9b2212229011876f27334d790572a3740218787
Iyana Medlock <iyanamedlock@gmail.com>
2021-02-05 20:53:22 -0600

    inquirer-recursive prompts installed and added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
 const { async } = require("rxjs");
 const inquirer = require("./node_modules/inquirer/lib/inquirer.js");
+inquirer.registerPrompt('recursive', require("./node_modules/inquirer-recursive"))
 
 inquirer.prompt([
   {
     type: "input",
-    message: "What is the project name?",
+    message: "What is the Project title?",
     name: "project",
+    validate: function(x){
+        if (x.length === 0){
+            return 'Please add Project Title'
+        }
+        return true;
+    }
   },
   {
     type: "checkbox",
@@ -69,5 +76,71 @@ inquirer.prompt([
       return false;
     },
   },
+  {
+    type: 'recursive',
+    message: 'Add another chapter? ',
+    name: 'addedSections',
+    prompts: [
+        {
+			type: 'input',
+			name: 'Section',
+			message: 'Chapter name: ',
+			
+		}, 
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'List the Features (separate with commas): ',
+            when: (answer) => {
+                if (answer.Section.includes("features") || answer.Section.includes('Features')) {
+                  return true;
+                }
+                return false;
+              },
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Badge URL (separate with commas): ',
+            when: (answer) => {
+                if (answer.Section.includes("badges") || answer.Section.includes('Badges')) {
+                  return true;
+                }
+                return false;
+              },
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Contributing (separate with commas): ',
+            when: (answer) => {
+                if (answer.Section.includes("contributing") || answer.Section.includes('Contributing')) {
+                  return true;
+                }
+                return false;
+              },
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Tests: (separate with commas): ',
+            when: (answer) => {
+                if (answer.Section.includes("tests") 
+                || answer.Section.includes('Tests') 
+                || answer.Section.includes('test') 
+                || answer.Section.includes('Test')) {
+                  return true;
+                }
+                return false;
+              },
+        },
+        {
+			type: 'input',
+			name: 'desc',
+			message: 'Chapter description: ',
+			
+		}, 
+    ]
+}
   
 ]);
